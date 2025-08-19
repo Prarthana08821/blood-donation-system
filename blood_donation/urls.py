db281589f8acbce9649b517f06c142a24d7bb047
Prarthana08821 <prarthanaj17@gmail.com>
2025-08-19 22:07:28 +0530

    Fix duplicate URL patterns for root path

--- blood_donation/urls.py
+++ blood_donation/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
+
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from donation import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Make sure this exists
+    path('', include('donation.urls')),  # Or include your app URLs
 ]
-
 
--- donation/urls.py
+++ donation/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # your existing URLs
+    path('', views.home, name='home'),  # Root URL - should be FIRST
+    path('home/', views.home, name='home'),  # Alternative home page
     path('test-pillow/', views.test_pillow_view, name='test_pillow'),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.user_logout, name='logout'),
+    path('donor-dashboard/', views.donor_dashboard, name='donor-dashboard'),
+    path('recipient-dashboard/', views.recipient_dashboard, name='recipient-dashboard'),
+    # Add all your other views here
 ]
+
+
